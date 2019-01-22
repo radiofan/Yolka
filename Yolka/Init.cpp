9fe6ec1f771f9c72e0658d8a75db8eb5a4aa92c7
Степан <43603990+radiofan@users.noreply.github.com>
2019-01-22 21:42:05 +0700

    Add files via upload

---
+++ Yolka/Init.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+using namespace std;
+
+void init(){
+    cout << "ษออออออออออออออออออออออป" << endl;
+    cout << "บ RADIOFAN GAME STUDIO บ" << endl;
+    cout << "ฬออออออออออออออออออออออน" << endl;
+    cout << "บ PROGRAM: X-MAS YOLKA บ" << endl;
+    cout << "วฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤฤถ" << endl;
+    cout << "บ Author: RADIOFAN     บ" << endl;
+    cout << "บ Version: 1.0         บ" << endl;
+    cout << "ศออออออออออออออออออออออผ" << endl;
+    cout << endl;
+}
---
+++ Yolka/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <cstdlib>
+#include <Windows.h>
+#include <iostream>//ввод вывод в консоль
+#include <random>
+//#include <string>
+
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
